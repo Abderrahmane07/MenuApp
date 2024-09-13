429c5a8c091e065ed9144a76ed896c8fc7241624
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2024-09-13 02:19:33 +0200

    Add other methods in Services for CRUD

--- MenuApp/Services/GeneralService.cs
+++ MenuApp/Services/GeneralService.cs
@@ -13,10 +13,44 @@ namespace MenuApp.Services
 			_context = context;
 		}
 
+		public async Task AddDishAsync(Dish dish)
+		{
+			_context.Dishes.Add(dish);	
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteDishAsync(int id)
+		{
+			var dish = await _context.Dishes.FindAsync(id);
+            if (dish != null)
+			{
+				_context.Dishes.Remove(dish);
+				await _context.SaveChangesAsync();
+			}
+        }
+
 		public async Task<List<Dish>> GetAllDishesAsync()
 		{ 
 			var result = await _context.Dishes.ToListAsync();
 			return result;
-		} 
+		}
+
+		public async Task<Dish> GetDishByIdAsync(int id)
+		{
+			var dish = await _context.Dishes.FindAsync(id);
+			return dish;
+		}
+
+		public async Task UpdateDishAsync(Dish dish, int id)
+		{
+			var dbDish = await _context.Dishes.FindAsync(id);
+			if (dbDish != null)
+			{
+				dbDish.Name = dish.Name; 
+				dbDish.Price = dish.Price; 
+
+				await _context.SaveChangesAsync();
+			}
+		}
 	}
 }
--- MenuApp/Services/IGeneralService.cs
+++ MenuApp/Services/IGeneralService.cs
@@ -5,5 +5,9 @@ namespace MenuApp.Services
 	public interface IGeneralService
 	{
 		Task<List<Dish>> GetAllDishesAsync();
+		Task<Dish> GetDishByIdAsync(int id);
+		Task AddDishAsync(Dish dish);
+		Task UpdateDishAsync(Dish dish, int id);
+		Task DeleteDishAsync(int id);
 	}
 }
