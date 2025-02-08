be38e2a2d4fad3b0c5ecfedcd7687b897666acc7
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2025-02-08 21:31:23 +0100

    Add pricing in the main menu

--- MenuApp/Services/QrCodeService.cs
+++ MenuApp/Services/QrCodeService.cs
@@ -19,14 +19,12 @@ namespace MenuApp.Services
 
 		public string GenerateQRCodeForMenu(int restaurantId)
 		{
-			// Generate the absolute URL for the menu
 			string menuUrl = _navigationManager.ToAbsoluteUri($"/menu/{restaurantId}").ToString();
 
-			// Generate QR code
 			using var qrGenerator = new QRCodeGenerator();
 			var qrCodeData = qrGenerator.CreateQrCode(menuUrl, QRCodeGenerator.ECCLevel.Q);
 			var qrCode = new BitmapByteQRCode(qrCodeData);
-			var qrCodeImage = qrCode.GetGraphic(20); // 20 is the pixel size per module
+			var qrCodeImage = qrCode.GetGraphic(20);
 
 			// Convert to base64 string for display in HTML
 			return $"data:image/png;base64,{Convert.ToBase64String(qrCodeImage)}";
